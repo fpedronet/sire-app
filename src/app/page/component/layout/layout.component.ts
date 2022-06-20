10ad89f57272629c20ab7949df7497ac16e008b8
fpedronet <36646447+fpedronet@users.noreply.github.com>
2022-06-20 15:03:05 -0500

    merge

--- src/app/page/component/layout/layout.component.ts
+++ src/app/page/component/layout/layout.component.ts
@@ -145,7 +145,6 @@ export class LayoutComponent implements OnInit {
       }else {
         let session = this.usuarioService.sessionUsuario();
         if(session==null){
-          this.timeLeft = 3602;
           clearInterval(this.interval);
           this.usuarioService.closeLogin();
         }
